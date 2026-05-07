26a08e8f1a943c6bee52bc1147b60e9428433853
ahmedhosnypro <ahhosnyas@gmail.com>
2026-05-07 18:47:29 +0300

    clean up the mess

--- backend/Domain/Entities/SystemSetting.cs
+++ backend/Domain/Entities/SystemSetting.cs
@@ -4,7 +4,7 @@ namespace Backend.Domain.Entities;
 
 public class SystemSetting : AuditableEntity
 {
-    public Guid Id { get; set; }
+    public new Guid Id { get; set; }
     public string Key { get; set; } = null!;
     public string Value { get; set; } = null!;
 }
